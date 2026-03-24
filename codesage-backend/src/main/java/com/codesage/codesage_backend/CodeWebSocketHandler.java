1d24cdbe6fcd77e1109a1682eb1372e25524fefc
”RAPARLAPRASANNAKUMAR” <prasannakumar0396@gmail.com>
2026-03-24 21:17:56 +0530

    updated codewebsocket

--- codesage-backend/src/main/java/com/codesage/codesage_backend/CodeWebSocketHandler.java
+++ codesage-backend/src/main/java/com/codesage/codesage_backend/CodeWebSocketHandler.java
@@ -117,7 +117,9 @@ public class CodeWebSocketHandler extends TextWebSocketHandler {
                 return;
             }
             
-            String apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=" + apiKey;
+            // This is the line that was fixed!
+            String apiUrl = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=" + apiKey;
+            
             String requestBody = "{\"contents\":[{\"parts\":[{\"text\": \"" + escapeJson(prompt) + "\"}]}]}";
 
             HttpRequest request = HttpRequest.newBuilder()
